27c5c59f4f9c5f11a2a80594b279a668d387e53c
Lohitnani <123918266+Lohitnani@users.noreply.github.com>
2023-10-13 00:03:05 +0530

    Add files via upload

---
+++ single inheritance.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class initial 
+{
+	public:
+		int x;
+		
+		void read()
+		{
+			cout<<"Enter the value of x";
+			cin>>x;
+		}
+		void display()
+		{
+			cout<<"X = "<<x<<"\n";
+		}
+};
+
+class final : public initial
+{
+	public:
+		int y;
+		
+		
+		
+		void read1()
+		{
+			cout<<"Enter the value of y";
+			cin>>y;
+		}
+		
+		void display1()
+		{
+			cout<<"Z = "<<x*y<<"\n";
+		}
+};
+
+int main()
+{
+	final F;
+	F.read();
+	F.read1();
+	F.display();
+	F.display1();
+	
+	return 0;
+}
